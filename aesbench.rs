d21c946f2bd7a8ac50bc3139e5573fedc48b1d9d
Palmer Cox <p@lmercox.com>
2013-07-04 15:03:28 -0400

    TMP: Update AES-NI, beginnings of AES-192, add aesbench.

---
+++ aesbench.rs
@@ -0,0 +1,61 @@
+extern mod test;
+
+use std::libc::*;
+use std::uint;
+
+use test::aesni::*;
+
+struct timespec {
+    tv_sec: time_t,
+    tv_nsec: c_long
+}
+
+type clockid_t = i32;
+
+static CLOCK_MONOTONIC: clockid_t = 1;
+
+extern {
+    unsafe fn clock_gettime(clk_id: clockid_t, tp: *timespec) -> c_int;
+}
+
+fn get_ns() -> i64 {
+    let ts = timespec {tv_sec: 0, tv_nsec: 0};
+    unsafe { clock_gettime(CLOCK_MONOTONIC, &ts); }
+    return ts.tv_sec * 1_000_000_000 + ts.tv_nsec;
+}
+
+fn main() {
+    let key128: [u8, ..16] = [0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c];
+    let key192: [u8, ..24] = [0x8e, 0x73, 0xb0, 0xf7, 0xda, 0x0e, 0x64, 0x52, 0xc8, 0x10, 0xf3, 0x2b, 0x80, 0x90, 0x79, 0xe5, 0x62, 0xf8, 0xea, 0xd2, 0x52, 0x2c, 0x6b, 0x7b];
+    let key256: [u8, ..32] = [0x60, 0x3d, 0xeb, 0x10, 0x15, 0xca, 0x71, 0xbe, 0x2b, 0x73, 0xae, 0xf0, 0x85, 0x7d, 0x77, 0x81, 0x1f, 0x35, 0x2c, 0x07, 0x3b, 0x61, 0x08, 0xd7, 0x2d, 0x98, 0x10, 0xa3, 0x09, 0x14, 0xdf, 0xf4];
+    let plain: [u8, ..16] = [0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a];
+    let cipher: [u8, ..16] = [0x3a, 0xd7, 0x7b, 0xb4, 0x0d, 0x7a, 0x36, 0x60, 0xa8, 0x9e, 0xca, 0xf3, 0x24, 0x66, 0xef, 0x97];
+
+    let mut enc = AesEncryptor::new();
+    enc.init(key128);
+
+    let mut dec = AesDecryptor::new();
+    dec.init(key128);
+
+    let mut out1 = [0u8, ..16];
+    let mut out2 = [0u8, ..16];
+
+    let count = 30000000;
+
+    let start_ns = get_ns();
+
+    for uint::range(0, count) |_| {
+        out1 = enc.encrypt_block(&plain);
+        out2 = dec.decrypt_block(&out1);
+    }
+
+    let end_ns = get_ns();
+
+    let total_ns = (end_ns - start_ns);
+    println(fmt!("time (ns): %?", total_ns));
+    let total_ms = (end_ns - start_ns) / 1_000_000;
+    println(fmt!("time (ms): %?", total_ms));
+    let size_mb = count * plain.len() / 1024 / 1024;
+    println(fmt!("size (mb): %?", size_mb));
+    println(fmt!("size (mb/s): %?", (size_mb as f64) / (total_ms as f64) * 1000f64 ));
+}
--- aesni.rs
+++ aesni.rs
@@ -10,8 +10,6 @@
 
 use std::uint;
 
-use symmetriccipher::*;
-
 /*
  * A Simple AES implementation using Intel AES-NI instructions
  */
@@ -26,7 +24,6 @@ pub trait SymmetricBlockDecryptor16 {
     fn decrypt_block(&mut self, in: &[u8, ..16]) -> [u8, ..16];
 }
 
-
 struct AesEncryptor {
     kw: [u8, ..16 * (10 + 1)]
 }
@@ -36,7 +33,7 @@ struct AesDecryptor {
 }
 
 impl AesEncryptor {
-    fn new() -> AesEncryptor {
+    pub fn new() -> AesEncryptor {
         return AesEncryptor {
             kw: ([0u8, ..16 * (10 + 1)])
         };
@@ -44,7 +41,7 @@ impl AesEncryptor {
 }
 
 impl AesDecryptor {
-    fn new() -> AesDecryptor {
+    pub fn new() -> AesDecryptor {
         return AesDecryptor {
             kw: ([0u8, ..16 * (10 + 1)])
         };
@@ -54,7 +51,7 @@ impl AesDecryptor {
 
 impl SymmetricBlockEncryptor16 for AesEncryptor {
     fn init(&mut self, key: &[u8]) {
-        self.kw = setup_working_key_aesni_128(key, 10, Encryption);
+        self.kw = setup_working_key_aesni_128(key, Encryption);
     }
 
     fn encrypt_block(&mut self, in: &[u8, ..16]) -> [u8, ..16] {
@@ -64,7 +61,7 @@ impl SymmetricBlockEncryptor16 for AesEncryptor {
 
 impl SymmetricBlockDecryptor16 for AesDecryptor {
     fn init(&mut self, key: &[u8]) {
-        self.kw = setup_working_key_aesni_128(key, 10, Decryption);
+        self.kw = setup_working_key_aesni_128(key, Decryption);
     }
 
     fn decrypt_block(&mut self, in: &[u8, ..16]) -> [u8, ..16] {
@@ -109,6 +106,7 @@ enum KeyType {
     Decryption
 }
 
+#[inline]
 unsafe fn aesimc(kw: *u8) {
     asm!(
     "
@@ -123,10 +121,7 @@ unsafe fn aesimc(kw: *u8) {
     )
 }
 
-fn setup_working_key_aesni_128(key: &[u8], rounds: uint, key_type: KeyType) -> [u8, ..16 * (10 + 1)] {
-    use std::cast::transmute;
-    use std::ptr::to_unsafe_ptr;
-
+fn setup_working_key_aesni_128(key: &[u8], key_type: KeyType) -> [u8, ..16 * (10 + 1)] {
     let kw = [0u8, ..16 * (10 + 1)];
 
     unsafe {
@@ -160,7 +155,7 @@ fn setup_working_key_aesni_128(key: &[u8], rounds: uint, key_type: KeyType) -> [
             aeskeygenassist $$0x36, %xmm1, %xmm2
             call key_expansion_128
 
-            jmp end
+            jmp end_key_128
 
             key_expansion_128:
             pshufd $$0xff, %xmm2, %xmm2
@@ -175,7 +170,7 @@ fn setup_working_key_aesni_128(key: &[u8], rounds: uint, key_type: KeyType) -> [
             add $$0x10, $0
             ret
 
-            end:
+            end_key_128:
         "
         : "=r" (kwp)
         : "r" (keyp), "0" (kwp)
@@ -196,6 +191,103 @@ fn setup_working_key_aesni_128(key: &[u8], rounds: uint, key_type: KeyType) -> [
 
     return kw;
 }
+
+#[cfg(off)]
+fn setup_working_key_aesni_192(key: &[u8], key_type: KeyType) -> [u8, ..16 * (12 + 1)] {
+    let kw = [0u8, ..16 * (12 + 1)];
+
+    unsafe {
+        let mut kwp: *u8 = kw.unsafe_ref(0);
+        let keyp: *u8 = key.unsafe_ref(0);
+
+        asm!(
+        "
+            movdqu ($1), %xmm1
+            movdqu %xmm1, ($0)
+            add $$0x10, $0
+
+                __m128i temp1, temp2, temp3, temp4;
+                __m128i *Key_Schedule = (__m128i*)key;
+                temp1 = _mm_loadu_si128((__m128i*)userkey);
+                temp3 = _mm_loadu_si128((__m128i*)(userkey+16));
+                Key_Schedule[0]=temp1;
+                Key_Schedule[1]=temp3;
+                temp2=_mm_aeskeygenassist_si128 (temp3,0x1);
+                KEY_192_ASSIST(&temp1, &temp2, &temp3);
+                Key_Schedule[1] = (__m128i)_mm_shuffle_pd((__m128d)Key_Schedule[1],
+                (__m128d)temp1,0);
+                Key_Schedule[2] = (__m128i)_mm_shuffle_pd((__m128d)temp1,(__m128d)temp3,1);
+                temp2=_mm_aeskeygenassist_si128 (temp3,0x2);
+                KEY_192_ASSIST(&temp1, &temp2, &temp3);
+                Key_Schedule[3]=temp1;
+                Key_Schedule[4]=temp3;
+                temp2=_mm_aeskeygenassist_si128 (temp3,0x4);
+                KEY_192_ASSIST(&temp1, &temp2, &temp3);
+                Key_Schedule[4] = (__m128i)_mm_shuffle_pd((__m128d)Key_Schedule[4],
+                (__m128d)temp1,0);
+                Key_Schedule[5] = (__m128i)_mm_shuffle_pd((__m128d)temp1,(__m128d)temp3,1);
+                temp2=_mm_aeskeygenassist_si128 (temp3,0x8);
+                KEY_192_ASSIST(&temp1, &temp2, &temp3);
+                Key_Schedule[6]=temp1;
+                Key_Schedule[7]=temp3;
+                temp2=_mm_aeskeygenassist_si128 (temp3,0x10);
+                KEY_192_ASSIST(&temp1, &temp2, &temp3);
+                Key_Schedule[7] = (__m128i)_mm_shuffle_pd((__m128d)Key_Schedule[7],
+                (__m128d)temp1,0);
+                Key_Schedule[8] = (__m128i)_mm_shuffle_pd((__m128d)temp1,(__m128d)temp3,1);
+                temp2=_mm_aeskeygenassist_si128 (temp3,0x20);
+                KEY_192_ASSIST(&temp1, &temp2, &temp3);
+                Key_Schedule[9]=temp1;
+                Key_Schedule[10]=temp3;
+                temp2=_mm_aeskeygenassist_si128 (temp3,0x40);
+                KEY_192_ASSIST(&temp1, &temp2, &temp3);
+                Key_Schedule[10] = (__m128i)_mm_shuffle_pd((__m128d)Key_Schedule[10],
+                (__m128d)temp1,0);
+                Key_Schedule[11] = (__m128i)_mm_shuffle_pd((__m128d)temp1,(__m128d)temp3,1);
+                temp2=_mm_aeskeygenassist_si128 (temp3,0x80);
+                KEY_192_ASSIST(&temp1, &temp2, &temp3);
+                Key_Schedule[12]=temp1;
+
+            jmp end_key_192
+
+            key_expansion_192:
+                __m128i temp4;
+                *temp2 = _mm_shuffle_epi32 (*temp2, 0x55);
+                temp4 = _mm_slli_si128 (*temp1, 0x4);
+                *temp1 = _mm_xor_si128 (*temp1, temp4);
+                temp4 = _mm_slli_si128 (temp4, 0x4);
+                *temp1 = _mm_xor_si128 (*temp1, temp4);
+                temp4 = _mm_slli_si128 (temp4, 0x4);
+                *temp1 = _mm_xor_si128 (*temp1, temp4);
+                *temp1 = _mm_xor_si128 (*temp1, *temp2);
+                *temp2 = _mm_shuffle_epi32(*temp1, 0xff);
+                temp4 = _mm_slli_si128 (*temp3, 0x4);
+                *temp3 = _mm_xor_si128 (*temp3, temp4);
+                *temp3 = _mm_xor_si128 (*temp3, *temp2);
+            ret
+
+            end_key_192:
+        "
+        : "=r" (kwp)
+        : "r" (keyp), "0" (kwp)
+        : "xmm1", "xmm2", "xmm3", "memory"
+        : "volatile"
+        )
+
+        match key_type {
+            Encryption => { /* nothing more to do */ }
+            Decryption => {
+                // range of rounds keys from #1 to #11; skip the first and last key
+                for uint::range(1, 12) |i| {
+                    aesimc(kw.unsafe_ref(16 * i));
+                }
+            }
+        }
+    }
+
+    return kw;
+}
+
 
 fn encrypt_block_aseni(rounds: uint, in: &[u8, ..16], kw: &[u8]) -> [u8, ..16] {
     use std::cast::transmute;
