a228e00ca7fd5877d222fa7b46134d8a6958276c
Palmer Cox <p@lmercox.com>
2013-07-28 22:41:04 -0400

    MD5 updates.

--- md5.rs
+++ md5.rs
@@ -14,43 +14,43 @@ use digest::Digest;
 
 // A structure that represents that state of a digest computation for the MD5 function
 struct Md5State {
-    H0: u32,
-    H1: u32,
-    H2: u32,
-    H3: u32
+    s0: u32,
+    s1: u32,
+    s2: u32,
+    s3: u32
 }
 
 impl Md5State {
     fn new() -> Md5State {
         return Md5State {
-            H0: 0x67452301,
-            H1: 0xefcdab89,
-            H2: 0x98badcfe,
-            H3: 0x10325476
+            s0: 0x67452301,
+            s1: 0xefcdab89,
+            s2: 0x98badcfe,
+            s3: 0x10325476
         };
     }
 
     fn reset(&mut self) {
-        self.H0 = 0x67452301;
-        self.H1 = 0xefcdab89;
-        self.H2 = 0x98badcfe;
-        self.H3 = 0x10325476;
+        self.s0 = 0x67452301;
+        self.s1 = 0xefcdab89;
+        self.s2 = 0x98badcfe;
+        self.s3 = 0x10325476;
     }
 
     fn process_block(&mut self, in: &[u8]) {
-        fn F(u: u32, v: u32, w: u32) -> u32 {
+        fn f(u: u32, v: u32, w: u32) -> u32 {
             return (u & v) | (!u & w);
         }
 
-        fn G(u: u32, v: u32, w: u32) -> u32 {
+        fn g(u: u32, v: u32, w: u32) -> u32 {
             return (u & w) | (v & !w);
         }
 
-        fn H(u: u32, v: u32, w: u32) -> u32 {
+        fn h(u: u32, v: u32, w: u32) -> u32 {
             return u ^ v ^ w;
         }
 
-        fn I(u: u32, v: u32, w: u32) -> u32 {
+        fn i(u: u32, v: u32, w: u32) -> u32 {
             return v ^ (u | !w);
         }
 
@@ -58,95 +58,107 @@ impl Md5State {
             return (x << n) | (x >> (32 - n));
         }
 
-        fn round(w: u32, x: u32, y: u32, z: u32, m: u32, s: u32) -> u32 {
-            return rotate_left(w + F(x, y, z) + m, s) + x;
+        fn op_f(w: u32, x: u32, y: u32, z: u32, m: u32, s: u32) -> u32 {
+            return rotate_left(w + f(x, y, z) + m, s) + x;
         }
 
-        let mut a = self.H0;
-        let mut b = self.H1;
-        let mut c = self.H2;
-        let mut d = self.H3;
+        fn op_g(w: u32, x: u32, y: u32, z: u32, m: u32, s: u32) -> u32 {
+            return rotate_left(w + g(x, y, z) + m, s) + x;
+        }
+
+        fn op_h(w: u32, x: u32, y: u32, z: u32, m: u32, s: u32) -> u32 {
+            return rotate_left(w + h(x, y, z) + m, s) + x;
+        }
+
+        fn op_i(w: u32, x: u32, y: u32, z: u32, m: u32, s: u32) -> u32 {
+            return rotate_left(w + i(x, y, z) + m, s) + x;
+        }
+
+        let mut a = self.s0;
+        let mut b = self.s1;
+        let mut c = self.s2;
+        let mut d = self.s3;
 
         let mut data = [0u32, ..16];
 
         read_u32v_le(data, in);
 
-        // Round 1 - F cycle, 16 times.
-        a = round(a, b, c, d, data[0] + 0xd76aa478, 7);
-        d = round(d, a, b, c, data[1] + 0xe8c7b756, 12);
-        c = round(c, d, a, b, data[2] + 0x242070db, 17);
-        b = round(b, c, d, a, data[3] + 0xc1bdceee, 22);
-        a = round(a, b, c, d, data[4] + 0xf57c0faf, 7);
-        d = round(d, a, b, c, data[5] + 0x4787c62a, 12);
-        c = round(c, d, a, b, data[6] + 0xa8304613, 17);
-        b = round(b, c, d, a, data[7] + 0xfd469501, 22);
-        a = round(a, b, c, d, data[8] + 0x698098d8, 7);
-        d = round(d, a, b, c, data[9] + 0x8b44f7af, 12);
-        c = round(c, d, a, b, data[10] + 0xffff5bb1, 17);
-        b = round(b, c, d, a, data[11] + 0x895cd7be, 22);
-        a = round(a, b, c, d, data[12] + 0x6b901122, 7);
-        d = round(d, a, b, c, data[13] + 0xfd987193, 12);
-        c = round(c, d, a, b, data[14] + 0xa679438e, 17);
-        b = round(b, c, d, a, data[15] + 0x49b40821, 22);
+        // round 1
+        a = op_f(a, b, c, d, data[0] + 0xd76aa478, 7);
+        d = op_f(d, a, b, c, data[1] + 0xe8c7b756, 12);
+        c = op_f(c, d, a, b, data[2] + 0x242070db, 17);
+        b = op_f(b, c, d, a, data[3] + 0xc1bdceee, 22);
+        a = op_f(a, b, c, d, data[4] + 0xf57c0faf, 7);
+        d = op_f(d, a, b, c, data[5] + 0x4787c62a, 12);
+        c = op_f(c, d, a, b, data[6] + 0xa8304613, 17);
+        b = op_f(b, c, d, a, data[7] + 0xfd469501, 22);
+        a = op_f(a, b, c, d, data[8] + 0x698098d8, 7);
+        d = op_f(d, a, b, c, data[9] + 0x8b44f7af, 12);
+        c = op_f(c, d, a, b, data[10] + 0xffff5bb1, 17);
+        b = op_f(b, c, d, a, data[11] + 0x895cd7be, 22);
+        a = op_f(a, b, c, d, data[12] + 0x6b901122, 7);
+        d = op_f(d, a, b, c, data[13] + 0xfd987193, 12);
+        c = op_f(c, d, a, b, data[14] + 0xa679438e, 17);
+        b = op_f(b, c, d, a, data[15] + 0x49b40821, 22);
 
-        // Round 2 - G cycle, 16 times.
-        a = rotate_left(a + G(b, c, d) + data[1] + 0xf61e2562, 5) + b;
-        d = rotate_left(d + G(a, b, c) + data[6] + 0xc040b340, 9) + a;
-        c = rotate_left(c + G(d, a, b) + data[11] + 0x265e5a51, 14) + d;
-        b = rotate_left(b + G(c, d, a) + data[0] + 0xe9b6c7aa, 20) + c;
-        a = rotate_left(a + G(b, c, d) + data[5] + 0xd62f105d, 5) + b;
-        d = rotate_left(d + G(a, b, c) + data[10] + 0x02441453, 9) + a;
-        c = rotate_left(c + G(d, a, b) + data[15] + 0xd8a1e681, 14) + d;
-        b = rotate_left(b + G(c, d, a) + data[4] + 0xe7d3fbc8, 20) + c;
-        a = rotate_left(a + G(b, c, d) + data[9] + 0x21e1cde6, 5) + b;
-        d = rotate_left(d + G(a, b, c) + data[14] + 0xc33707d6, 9) + a;
-        c = rotate_left(c + G(d, a, b) + data[3] + 0xf4d50d87, 14) + d;
-        b = rotate_left(b + G(c, d, a) + data[8] + 0x455a14ed, 20) + c;
-        a = rotate_left(a + G(b, c, d) + data[13] + 0xa9e3e905, 5) + b;
-        d = rotate_left(d + G(a, b, c) + data[2] + 0xfcefa3f8, 9) + a;
-        c = rotate_left(c + G(d, a, b) + data[7] + 0x676f02d9, 14) + d;
-        b = rotate_left(b + G(c, d, a) + data[12] + 0x8d2a4c8a, 20) + c;
+        // round 2
+        a = op_g(a, b, c, d, data[1] + 0xf61e2562, 5);
+        d = op_g(d, a, b, c, data[6] + 0xc040b340, 9);
+        c = op_g(c, d, a, b, data[11] + 0x265e5a51, 14);
+        b = op_g(b, c, d, a, data[0] + 0xe9b6c7aa, 20);
+        a = op_g(a, b, c, d, data[5] + 0xd62f105d, 5);
+        d = op_g(d, a, b, c, data[10] + 0x02441453, 9);
+        c = op_g(c, d, a, b, data[15] + 0xd8a1e681, 14);
+        b = op_g(b, c, d, a, data[4] + 0xe7d3fbc8, 20);
+        a = op_g(a, b, c, d, data[9] + 0x21e1cde6, 5);
+        d = op_g(d, a, b, c, data[14] + 0xc33707d6, 9);
+        c = op_g(c, d, a, b, data[3] + 0xf4d50d87, 14);
+        b = op_g(b, c, d, a, data[8] + 0x455a14ed, 20);
+        a = op_g(a, b, c, d, data[13] + 0xa9e3e905, 5);
+        d = op_g(d, a, b, c, data[2] + 0xfcefa3f8, 9);
+        c = op_g(c, d, a, b, data[7] + 0x676f02d9, 14);
+        b = op_g(b, c, d, a, data[12] + 0x8d2a4c8a, 20);
 
-        // Round 3 - H cycle, 16 times.
-        a = rotate_left(a + H(b, c, d) + data[5] + 0xfffa3942, 4) + b;
-        d = rotate_left(d + H(a, b, c) + data[8] + 0x8771f681, 11) + a;
-        c = rotate_left(c + H(d, a, b) + data[11] + 0x6d9d6122, 16) + d;
-        b = rotate_left(b + H(c, d, a) + data[14] + 0xfde5380c, 23) + c;
-        a = rotate_left(a + H(b, c, d) + data[1] + 0xa4beea44, 4) + b;
-        d = rotate_left(d + H(a, b, c) + data[4] + 0x4bdecfa9, 11) + a;
-        c = rotate_left(c + H(d, a, b) + data[7] + 0xf6bb4b60, 16) + d;
-        b = rotate_left(b + H(c, d, a) + data[10] + 0xbebfbc70, 23) + c;
-        a = rotate_left(a + H(b, c, d) + data[13] + 0x289b7ec6, 4) + b;
-        d = rotate_left(d + H(a, b, c) + data[0] + 0xeaa127fa, 11) + a;
-        c = rotate_left(c + H(d, a, b) + data[3] + 0xd4ef3085, 16) + d;
-        b = rotate_left(b + H(c, d, a) + data[6] + 0x04881d05, 23) + c;
-        a = rotate_left(a + H(b, c, d) + data[9] + 0xd9d4d039, 4) + b;
-        d = rotate_left(d + H(a, b, c) + data[12] + 0xe6db99e5, 11) + a;
-        c = rotate_left(c + H(d, a, b) + data[15] + 0x1fa27cf8, 16) + d;
-        b = rotate_left(b + H(c, d, a) + data[2] + 0xc4ac5665, 23) + c;
+        // round 3
+        a = op_h(a, b, c, d, data[5] + 0xfffa3942, 4);
+        d = op_h(d, a, b, c, data[8] + 0x8771f681, 11);
+        c = op_h(c, d, a, b, data[11] + 0x6d9d6122, 16);
+        b = op_h(b, c, d, a, data[14] + 0xfde5380c, 23);
+        a = op_h(a, b, c, d, data[1] + 0xa4beea44, 4);
+        d = op_h(d, a, b, c, data[4] + 0x4bdecfa9, 11);
+        c = op_h(c, d, a, b, data[7] + 0xf6bb4b60, 16);
+        b = op_h(b, c, d, a, data[10] + 0xbebfbc70, 23);
+        a = op_h(a, b, c, d, data[13] + 0x289b7ec6, 4);
+        d = op_h(d, a, b, c, data[0] + 0xeaa127fa, 11);
+        c = op_h(c, d, a, b, data[3] + 0xd4ef3085, 16);
+        b = op_h(b, c, d, a, data[6] + 0x04881d05, 23);
+        a = op_h(a, b, c, d, data[9] + 0xd9d4d039, 4);
+        d = op_h(d, a, b, c, data[12] + 0xe6db99e5, 11);
+        c = op_h(c, d, a, b, data[15] + 0x1fa27cf8, 16);
+        b = op_h(b, c, d, a, data[2] + 0xc4ac5665, 23);
 
-        // Round 4 - K cycle, 16 times.
-        a = rotate_left(a + I(b, c, d) + data[0] + 0xf4292244, 6) + b;
-        d = rotate_left(d + I(a, b, c) + data[7] + 0x432aff97, 10) + a;
-        c = rotate_left(c + I(d, a, b) + data[14] + 0xab9423a7, 15) + d;
-        b = rotate_left(b + I(c, d, a) + data[5] + 0xfc93a039, 21) + c;
-        a = rotate_left(a + I(b, c, d) + data[12] + 0x655b59c3, 6) + b;
-        d = rotate_left(d + I(a, b, c) + data[3] + 0x8f0ccc92, 10) + a;
-        c = rotate_left(c + I(d, a, b) + data[10] + 0xffeff47d, 15) + d;
-        b = rotate_left(b + I(c, d, a) + data[1] + 0x85845dd1, 21) + c;
-        a = rotate_left(a + I(b, c, d) + data[8] + 0x6fa87e4f, 6) + b;
-        d = rotate_left(d + I(a, b, c) + data[15] + 0xfe2ce6e0, 10) + a;
-        c = rotate_left(c + I(d, a, b) + data[6] + 0xa3014314, 15) + d;
-        b = rotate_left(b + I(c, d, a) + data[13] + 0x4e0811a1, 21) + c;
-        a = rotate_left(a + I(b, c, d) + data[4] + 0xf7537e82, 6) + b;
-        d = rotate_left(d + I(a, b, c) + data[11] + 0xbd3af235, 10) + a;
-        c = rotate_left(c + I(d, a, b) + data[2] + 0x2ad7d2bb, 15) + d;
-        b = rotate_left(b + I(c, d, a) + data[9] + 0xeb86d391, 21) + c;
+        // round 4
+        a = op_i(a, b, c, d, data[0] + 0xf4292244, 6);
+        d = op_i(d, a, b, c, data[7] + 0x432aff97, 10);
+        c = op_i(c, d, a, b, data[14] + 0xab9423a7, 15);
+        b = op_i(b, c, d, a, data[5] + 0xfc93a039, 21);
+        a = op_i(a, b, c, d, data[12] + 0x655b59c3, 6);
+        d = op_i(d, a, b, c, data[3] + 0x8f0ccc92, 10);
+        c = op_i(c, d, a, b, data[10] + 0xffeff47d, 15);
+        b = op_i(b, c, d, a, data[1] + 0x85845dd1, 21);
+        a = op_i(a, b, c, d, data[8] + 0x6fa87e4f, 6);
+        d = op_i(d, a, b, c, data[15] + 0xfe2ce6e0, 10);
+        c = op_i(c, d, a, b, data[6] + 0xa3014314, 15);
+        b = op_i(b, c, d, a, data[13] + 0x4e0811a1, 21);
+        a = op_i(a, b, c, d, data[4] + 0xf7537e82, 6);
+        d = op_i(d, a, b, c, data[11] + 0xbd3af235, 10);
+        c = op_i(c, d, a, b, data[2] + 0x2ad7d2bb, 15);
+        b = op_i(b, c, d, a, data[9] + 0xeb86d391, 21);
 
-        self.H0 += a;
-        self.H1 += b;
-        self.H2 += c;
-        self.H3 += d;
+        self.s0 += a;
+        self.s1 += b;
+        self.s2 += c;
+        self.s3 += d;
     }
 }
 
@@ -196,10 +208,10 @@ impl Digest for Md5 {
             self.finished = true;
         }
 
-        write_u32_le(out.mut_slice(0, 4), self.state.H0);
-        write_u32_le(out.mut_slice(4, 8), self.state.H1);
-        write_u32_le(out.mut_slice(8, 12), self.state.H2);
-        write_u32_le(out.mut_slice(12, 16), self.state.H3);
+        write_u32_le(out.mut_slice(0, 4), self.state.s0);
+        write_u32_le(out.mut_slice(4, 8), self.state.s1);
+        write_u32_le(out.mut_slice(8, 12), self.state.s2);
+        write_u32_le(out.mut_slice(12, 16), self.state.s3);
     }
 
     fn output_bits(&self) -> uint { 128 }
