2f113de60eab7fdc8fec1f9e2922dd137ef7f41a
Palmer Cox <p@lmercox.com>
2013-08-04 15:57:34 -0400

    AesNi - working for 192 and 256 bit versions

--- aesni.rs
+++ aesni.rs
@@ -8,9 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use std::vec::bytes;
-
 use symmetriccipher::*;
+
 
 pub struct AesNi128Encryptor {
     priv round_keys: [u8, ..16 * (10 + 1)]
@@ -161,13 +160,13 @@ enum KeyType {
     Decryption
 }
 
-#[inline]
+#[inline(always)]
 unsafe fn aesimc(round_keys: *u8) {
     asm!(
     "
-    movdqu ($0), %xmm1
-    aesimc %xmm1, %xmm1
-    movdqu %xmm1, ($0)
+        movdqu ($0), %xmm1
+        aesimc %xmm1, %xmm1
+        movdqu %xmm1, ($0)
     "
     : // outputs
     : "r" (round_keys) // inputs
@@ -179,16 +178,14 @@ unsafe fn aesimc(round_keys: *u8) {
 #[inline(never)]
 fn setup_working_key_aesni_128(key: &[u8], key_type: KeyType, round_key: &mut [u8]) {
     unsafe {
-        // copy the key into the round_key
-        bytes::copy_memory(round_key, key, key.len());
-
         let mut round_keysp: *u8 = round_key.unsafe_ref(0);
         let keyp: *u8 = key.unsafe_ref(0);
 
         asm!(
         "
             movdqu ($1), %xmm1
-            add $$0x10, $0 /* skip over the bytes we already copied */
+            movdqu %xmm1, ($0)
+            add $$0x10, $0
 
             aeskeygenassist $$0x01, %xmm1, %xmm2
             call key_expansion_128
@@ -246,10 +243,217 @@ fn setup_working_key_aesni_128(key: &[u8], key_type: KeyType, round_key: &mut [u
     }
 }
 
+#[inline(never)]
 fn setup_working_key_aesni_192(key: &[u8], key_type: KeyType, round_key: &mut [u8]) {
+    unsafe {
+        let mut round_keysp: *u8 = round_key.unsafe_ref(0);
+        let keyp: *u8 = key.unsafe_ref(0);
+
+        asm!(
+        "
+            movdqu ($1), %xmm1
+            movdqu 16($1), %xmm3
+            movdqu %xmm1, ($0)
+            movdqa %xmm3, %xmm5
+
+            aeskeygenassist $$0x1, %xmm3, %xmm2
+            call process_key_192
+            shufpd $$0, %xmm1, %xmm5
+            movdqu %xmm5, 16($0)
+            movdqa %xmm1, %xmm6
+            shufpd $$1, %xmm3, %xmm6
+            movdqu %xmm6, 32($0)
+
+            aeskeygenassist $$0x2, %xmm3, %xmm2
+            call process_key_192
+            movdqu %xmm1, 48($0)
+            movdqa %xmm3, %xmm5
+
+            aeskeygenassist $$0x4, %xmm3, %xmm2
+            call process_key_192
+            shufpd $$0, %xmm1, %xmm5
+            movdqu %xmm5, 64($0)
+            movdqa %xmm1, %xmm6
+            shufpd $$1, %xmm3, %xmm6
+            movdqu %xmm6, 80($0)
+
+            aeskeygenassist $$0x8, %xmm3, %xmm2
+            call process_key_192
+            movdqu %xmm1, 96($0)
+            movdqa %xmm3, %xmm5
+
+            aeskeygenassist $$0x10, %xmm3, %xmm2
+            call process_key_192
+            shufpd $$0, %xmm1, %xmm5
+            movdqu %xmm5, 112($0)
+            movdqa %xmm1, %xmm6
+            shufpd $$1, %xmm3, %xmm6
+            movdqu %xmm6, 128($0)
+
+            aeskeygenassist $$0x20, %xmm3, %xmm2
+            call process_key_192
+            movdqu %xmm1, 144($0)
+            movdqa %xmm3, %xmm5
+
+            aeskeygenassist $$0x40, %xmm3, %xmm2
+            call process_key_192
+            shufpd $$0, %xmm1, %xmm5
+            movdqu %xmm5, 160($0)
+            movdqa %xmm1, %xmm6
+            shufpd $$1, %xmm3, %xmm6
+            movdqu %xmm6, 176($0)
+
+            aeskeygenassist $$0x80, %xmm3, %xmm2
+            call process_key_192
+            movdqu %xmm1, 192($0)
+
+            jmp end_key_192
+
+            process_key_192:
+            pshufd $$0x55, %xmm2, %xmm2
+            movdqu %xmm1, %xmm4
+            pslldq $$4, %xmm4
+            pxor %xmm4, %xmm1
+            pslldq $$4, %xmm4
+            pxor %xmm4, %xmm1
+            pslldq $$4, %xmm4
+            pxor %xmm4, %xmm1
+            pxor %xmm2, %xmm1
+            pshufd $$0xff, %xmm1, %xmm2
+            movdqu %xmm3, %xmm4
+            pslldq $$4, %xmm4
+            pxor %xmm4, %xmm3
+            pxor %xmm2, %xmm3
+            ret
+
+            end_key_192:
+        "
+        : "=r" (round_keysp)
+        : "r" (keyp), "0" (round_keysp)
+        : "xmm1", "xmm2", "xmm3", "xmm4", "xmm5", "xmm6", "memory"
+        : "volatile"
+        )
+
+        match key_type {
+            Decryption => {
+                // range of rounds keys from #1 to #11; skip the first and last key
+                for i in range(1u, 12) {
+                    aesimc(round_key.unsafe_ref(16 * i));
+                }
+            }
+            Encryption => { /* nothing more to do */ }
+        }
+    }
 }
 
 fn setup_working_key_aesni_256(key: &[u8], key_type: KeyType, round_key: &mut [u8]) {
+    unsafe {
+        let mut round_keysp: *u8 = round_key.unsafe_ref(0);
+        let keyp: *u8 = key.unsafe_ref(0);
+
+        asm!(
+        "
+            movdqu ($1), %xmm1
+            movdqu 16($1), %xmm3
+            movdqu %xmm1, ($0)
+            movdqu %xmm3, 16($0)
+
+            aeskeygenassist $$0x1, %xmm3, %xmm2
+            call make_rk256_a
+            movdqu %xmm1, 32($0)
+
+            aeskeygenassist $$0x0, %xmm1, %xmm2
+            call make_rk256_b
+            movdqu %xmm3, 48($0)
+
+            aeskeygenassist $$0x2, %xmm3, %xmm2
+            call make_rk256_a
+            movdqu %xmm1, 64($0)
+
+            aeskeygenassist $$0x0, %xmm1, %xmm2
+            call make_rk256_b
+            movdqu %xmm3, 80($0)
+
+            aeskeygenassist $$0x4, %xmm3, %xmm2
+            call make_rk256_a
+            movdqu %xmm1, 96($0)
+
+            aeskeygenassist $$0x0, %xmm1, %xmm2
+            call make_rk256_b
+            movdqu %xmm3, 112($0)
+
+            aeskeygenassist $$0x8, %xmm3, %xmm2
+            call make_rk256_a
+            movdqu %xmm1, 128($0)
+
+            aeskeygenassist $$0x0, %xmm1, %xmm2
+            call make_rk256_b
+            movdqu %xmm3, 144($0)
+
+            aeskeygenassist $$0x10, %xmm3, %xmm2
+            call make_rk256_a
+            movdqu %xmm1, 160($0)
+
+            aeskeygenassist $$0x0, %xmm1, %xmm2
+            call make_rk256_b
+            movdqu %xmm3, 176($0)
+
+            aeskeygenassist $$0x20, %xmm3, %xmm2
+            call make_rk256_a
+            movdqu %xmm1, 192($0)
+
+            aeskeygenassist $$0x0, %xmm1, %xmm2
+            call make_rk256_b
+            movdqu %xmm3, 208($0)
+
+            aeskeygenassist $$0x40, %xmm3, %xmm2
+            call make_rk256_a
+            movdqu %xmm1, 224($0)
+
+            jmp end_key_256
+
+            make_rk256_a:
+            pshufd $$0xff, %xmm2, %xmm2
+            movdqa %xmm1, %xmm4
+            pslldq $$4, %xmm4
+            pxor %xmm4, %xmm1
+            pslldq $$4, %xmm4
+            pxor %xmm4, %xmm1
+            pslldq $$4, %xmm4
+            pxor %xmm4, %xmm1
+            pxor %xmm2, %xmm1
+            ret
+
+            make_rk256_b:
+            pshufd $$0xaa, %xmm2, %xmm2
+            movdqa %xmm3, %xmm4
+            pslldq $$4, %xmm4
+            pxor %xmm4, %xmm3
+            pslldq $$4, %xmm4
+            pxor %xmm4, %xmm3
+            pslldq $$4, %xmm4
+            pxor %xmm4, %xmm3
+            pxor %xmm2, %xmm3
+            ret
+
+            end_key_256:
+        "
+        : "=r" (round_keysp)
+        : "r" (keyp), "0" (round_keysp)
+        : "xmm1", "xmm2", "xmm3", "xmm4", "memory"
+        : "volatile"
+        )
+
+        match key_type {
+            Decryption => {
+                // range of rounds keys from #1 to #13; skip the first and last key
+                for i in range(1u, 14) {
+                    aesimc(round_key.unsafe_ref(16 * i));
+                }
+            }
+            Encryption => { /* nothing more to do */ }
+        }
+    }
 }
 
 #[inline(never)]
@@ -262,29 +466,29 @@ fn encrypt_block_aseni(rounds: uint, input: &[u8], round_keys: &[u8], output: &m
 
         asm!(
         "
-        /* Copy the data to encrypt to xmm15 */
-        movdqu ($2), %xmm15
+            /* Copy the data to encrypt to xmm15 */
+            movdqu ($2), %xmm15
 
-        /* Perform round 0 - the whitening step */
-        movdqu ($1), %xmm0
-        add $$0x10, $1
-        pxor %xmm0, %xmm15
+            /* Perform round 0 - the whitening step */
+            movdqu ($1), %xmm0
+            add $$0x10, $1
+            pxor %xmm0, %xmm15
 
-        /* Perform all remaining rounds (except the final one) */
-        enc_round:
-        movdqu ($1), %xmm0
-        add $$0x10, $1
-        aesenc %xmm0, %xmm15
-        sub $$0x01, $0
-        cmp $$0x01, $0
-        jne enc_round
+            /* Perform all remaining rounds (except the final one) */
+            enc_round:
+            movdqu ($1), %xmm0
+            add $$0x10, $1
+            aesenc %xmm0, %xmm15
+            sub $$0x01, $0
+            cmp $$0x01, $0
+            jne enc_round
 
-        /* Perform the last round */
-        movdqu ($1), %xmm0
-        aesenclast %xmm0, %xmm15
+            /* Perform the last round */
+            movdqu ($1), %xmm0
+            aesenclast %xmm0, %xmm15
 
-        /* Finally, move the result from xmm15 to outp */
-        movdqu %xmm15, ($3)
+            /* Finally, move the result from xmm15 to outp */
+            movdqu %xmm15, ($3)
         "
         : "=r" (rounds), "=r" (round_keysp) // outputs
         : "r" (inp), "r" (outp), "0" (rounds), "1" (round_keysp) // inputs
@@ -304,29 +508,29 @@ fn decrypt_block_aseni(rounds: uint, input: &[u8], round_keys: &[u8], output: &m
 
         asm!(
         "
-        /* Copy the data to decrypt to xmm15 */
-        movdqu ($2), %xmm15
+            /* Copy the data to decrypt to xmm15 */
+            movdqu ($2), %xmm15
 
-        /* Perform round 0 - the whitening step */
-        movdqu ($1), %xmm0
-        sub $$0x10, $1
-        pxor %xmm0, %xmm15
+            /* Perform round 0 - the whitening step */
+            movdqu ($1), %xmm0
+            sub $$0x10, $1
+            pxor %xmm0, %xmm15
 
-        /* Perform all remaining rounds (except the final one) */
-        dec_round:
-        movdqu ($1), %xmm0
-        sub $$0x10, $1
-        aesdec %xmm0, %xmm15
-        sub $$0x01, $0
-        cmp $$0x01, $0
-        jne dec_round
+            /* Perform all remaining rounds (except the final one) */
+            dec_round:
+            movdqu ($1), %xmm0
+            sub $$0x10, $1
+            aesdec %xmm0, %xmm15
+            sub $$0x01, $0
+            cmp $$0x01, $0
+            jne dec_round
 
-        /* Perform the last round */
-        movdqu ($1), %xmm0
-        aesdeclast %xmm0, %xmm15
+            /* Perform the last round */
+            movdqu ($1), %xmm0
+            aesdeclast %xmm0, %xmm15
 
-        /* Finally, move the result from xmm15 to outp */
-        movdqu %xmm15, ($3)
+            /* Finally, move the result from xmm15 to outp */
+            movdqu %xmm15, ($3)
         "
         : "=r" (rounds), "=r" (round_keysp) // outputs
         : "r" (inp), "r" (outp), "0" (rounds), "1" (round_keysp) // inputs
