023f3792d975c9bbda4350d0bd58bdb2945ad6de
Palmer Cox <p@lmercox.com>
2013-08-13 23:36:17 -0400

    Update AesSafe implementation to use bit slicing method to calculate 8 sboxes at once.

--- aessafe.rs
+++ aessafe.rs
@@ -92,20 +92,6 @@ define_aes_enc!(AesSafe256Encryptor, 14)
 define_aes_dec!(AesSafe256Decryptor, 14)
 
 
-/// Get the value from the specified index using a fixed number of instructions
-fn fixed_get(v: &[u8], idx: uint) -> u32 {
-    let mut out: u32 = 0;
-    for i in range(0, v.len()) {
-        out = (i as u8).fixed_eq(idx as u8).fixed_select(v[i] as u32, out);
-    }
-    return out;
-}
-
-/// Get the RCON value at the specified index using a fixed number of instructions
-fn rcon(idx: uint) -> u32 {
-    return fixed_get(RCON, idx as uint);
-}
-
 fn shift(r: u32, shift: u32) -> u32 {
     return (r >> shift) | (r << (32 - shift));
 }
@@ -167,7 +153,7 @@ fn setup_round_keys(key: &[u8], key_type: KeyType, round_keys: &mut [[u32, ..4]]
     for i in range(key_words, (rounds + 1) * 4) {
         let mut temp = round_keys[(i - 1) / 4][(i - 1) % 4];
         if (i % key_words) == 0 {
-            temp = sub_word(shift(temp, 8)) ^ rcon((i / key_words) - 1);
+            temp = sub_word(shift(temp, 8)) ^ RCON[(i / key_words) - 1];
         } else if (key_words == 8) && ((i % key_words) == 4) {
             // This is only necessary for AES-256 keys
             temp = sub_word(temp);
@@ -190,17 +176,12 @@ fn setup_round_keys(key: &[u8], key_type: KeyType, round_keys: &mut [[u32, ..4]]
 
 fn encrypt_block(rounds: uint, input: &[u8], rk: &[[u32, ..4]], output: &mut [u8]) {
     fn op(v: u32, x: u32, y: u32, z: u32, k: u32) -> u32 {
-        return mcol(s(v) ^ (s(x >> 8) << 8) ^ (s(y >> 16) << 16) ^ (s(z >> 24) << 24)) ^ k;
+        return mcol((v & 0xff) ^ (x & 0xff00) ^ (y & 0xff0000) ^ (z & 0xff000000)) ^ k;
     }
 
     fn op_end(v: u32, x: u32, y: u32, z: u32, k: u32) -> u32 {
-        return s(v) ^ (s(x >> 8) << 8) ^ (s(y >> 16) << 16) ^ (s(z >> 24) << 24) ^ k;
+        return (v & 0xff) ^ (x & 0xff00) ^ (y & 0xff0000) ^ (z & 0xff000000) ^ k;
     }
-
-    let mut r0: u32;
-    let mut r1: u32;
-    let mut r2: u32;
-    let mut r3: u32;
 
     let mut c = [0u32, ..4];
     read_u32v_le(c, input);
@@ -217,12 +198,16 @@ fn encrypt_block(rounds: uint, input: &[u8], rk: &[[u32, ..4]], output: &mut [u8
 
     let mut r = 1;
     while (r < rounds - 1) {
-        r0 = op(c0, c1, c2, c3, rk[r][0]);
-        r1 = op(c1, c2, c3, c0, rk[r][1]);
-        r2 = op(c2, c3, c0, c1, rk[r][2]);
-        r3 = op(c3, c0, c1, c2, rk[r][3]);
+        unpack(Sbox(pack(c0, c1)), &mut c0, &mut c1);
+        unpack(Sbox(pack(c2, c3)), &mut c2, &mut c3);
+        let mut r0 = op(c0, c1, c2, c3, rk[r][0]);
+        let mut r1 = op(c1, c2, c3, c0, rk[r][1]);
+        let mut r2 = op(c2, c3, c0, c1, rk[r][2]);
+        let mut r3 = op(c3, c0, c1, c2, rk[r][3]);
         r += 1;
 
+        unpack(Sbox(pack(r0, r1)), &mut r0, &mut r1);
+        unpack(Sbox(pack(r2, r3)), &mut r2, &mut r3);
         c0 = op(r0, r1, r2, r3, rk[r][0]);
         c1 = op(r1, r2, r3, r0, rk[r][1]);
         c2 = op(r2, r3, r0, r1, rk[r][2]);
@@ -230,12 +215,16 @@ fn encrypt_block(rounds: uint, input: &[u8], rk: &[[u32, ..4]], output: &mut [u8
         r += 1;
     }
 
-    r0 = op(c0, c1, c2, c3, rk[r][0]);
-    r1 = op(c1, c2, c3, c0, rk[r][1]);
-    r2 = op(c2, c3, c0, c1, rk[r][2]);
-    r3 = op(c3, c0, c1, c2, rk[r][3]);
+    unpack(Sbox(pack(c0, c1)), &mut c0, &mut c1);
+    unpack(Sbox(pack(c2, c3)), &mut c2, &mut c3);
+    let mut r0 = op(c0, c1, c2, c3, rk[r][0]);
+    let mut r1 = op(c1, c2, c3, c0, rk[r][1]);
+    let mut r2 = op(c2, c3, c0, c1, rk[r][2]);
+    let mut r3 = op(c3, c0, c1, c2, rk[r][3]);
     r += 1;
 
+    unpack(Sbox(pack(r0, r1)), &mut r0, &mut r1);
+    unpack(Sbox(pack(r2, r3)), &mut r2, &mut r3);
     c0 = op_end(r0, r1, r2, r3, rk[r][0]);
     c1 = op_end(r1, r2, r3, r0, rk[r][1]);
     c2 = op_end(r2, r3, r0, r1, rk[r][2]);
@@ -340,6 +329,7 @@ fn sbox(x: u8) -> u8 {
 /// some casts
 fn s(idx: u32) -> u32 {
     return sbox(idx as u8) as u32;
+//    return Sbox(idx as u8) as u32;
 }
 
 fn s_inv_box(x: u8) -> u8 {
@@ -361,7 +351,171 @@ fn s_inv(idx: u32) -> u32 {
     return s_inv_box(idx as u8) as u32;
 }
 
-static RCON: [u8, ..10] = [
+static RCON: [u32, ..10] = [
     0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80,
     0x1b, 0x36
 ];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// to convert between polynomial (A^7...1) basis A & normal basis X
+// or to basis S which incorporates bit matrix of Sbox
+static A2X: [u64, ..8] = [0x9898989898989898, 0xF3F3F3F3F3F3F3F3, 0xF2F2F2F2F2F2F2F2, 0x4848484848484848, 0x0909090909090909, 0x8181818181818181, 0xA9A9A9A9A9A9A9A9, 0xFFFFFFFFFFFFFFFF];
+static X2A: [u64, ..8] = [0x6464646464646464, 0x7878787878787878, 0x6E6E6E6E6E6E6E6E, 0x8C8C8C8C8C8C8C8C, 0x6868686868686868, 0x2929292929292929, 0xDEDEDEDEDEDEDEDE, 0x6060606060606060];
+static X2S: [u64, ..8] = [0x5858585858585858, 0x2D2D2D2D2D2D2D2D, 0x9E9E9E9E9E9E9E9E, 0x0B0B0B0B0B0B0B0B, 0xDCDCDCDCDCDCDCDC, 0x0404040404040404, 0x0303030303030303, 0x2424242424242424];
+static S2X: [u64, ..8] = [0x8C8C8C8C8C8C8C8C, 0x7979797979797979, 0x0505050505050505, 0xEBEBEBEBEBEBEBEB, 0x1212121212121212, 0x0404040404040404, 0x5151515151515151, 0x5353535353535353];
+
+// multiply in GF(2^2), using normal basis (Omega^2,Omega)
+fn G4_mul(x: u64, y: u64) -> u64 {
+    let a = (x & 0x0202020202020202) >> 1;
+    let b = (x & 0x0101010101010101);
+    let c = (y & 0x0202020202020202) >> 1;
+    let d = (y & 0x0101010101010101);
+    let e = (a ^ b) & (c ^ d);
+    let p = (a & c) ^ e;
+    let q = (b & d) ^ e;
+    return (p << 1) | q;
+}
+
+// scale by N = Omega^2 in GF(2^2), using normal basis (Omega^2,Omega)
+fn G4_scl_N(x: u64) -> u64 {
+    let a = (x & 0x0202020202020202) >> 1;
+    let b = (x & 0x0101010101010101);
+    let p = b;
+    let q = a ^ b;
+    return (p << 1) | q;
+}
+
+// scale by N^2 = Omega in GF(2^2), using normal basis (Omega^2,Omega)
+fn G4_scl_N2(x: u64) -> u64 {
+    let a = (x & 0x0202020202020202) >> 1;
+    let b = (x & 0x0101010101010101);
+    let p = a ^ b;
+    let q = a;
+    return (p << 1) | q;
+}
+
+// square in GF(2^2), using normal basis (Omega^2,Omega)
+// NOTE: inverse is identical
+fn G4_sq(x: u64) -> u64 {
+    let a = (x & 0x0202020202020202) >> 1;
+    let b = (x & 0x0101010101010101);
+    return (b << 1) | a;
+}
+
+fn G4_inv(x: u64) -> u64 {
+    // Same as sqaure
+    return G4_sq(x);
+}
+
+// multiply in GF(2^4), using normal basis (alpha^8,alpha^2)
+fn G16_mul(x: u64, y: u64) -> u64 {
+    let a = (x & 0x0C0C0C0C0C0C0C0C) >> 2;
+    let b = (x & 0x0303030303030303);
+    let c = (y & 0x0C0C0C0C0C0C0C0C) >> 2;
+    let d = (y & 0x0303030303030303);
+    let e = G4_mul(a ^ b, c ^ d);
+    let e = G4_scl_N(e);
+    let p = G4_mul(a, c) ^ e;
+    let q = G4_mul(b, d) ^ e;
+    return (p << 2) | q;
+}
+
+// square & scale by nu in GF(2^4)/GF(2^2), normal basis (alpha^8,alpha^2)
+// nu = beta^8 = N^2*alpha^2, N = w^2 */
+fn G16_sq_scl(x: u64) -> u64 {
+    let a = (x & 0x0C0C0C0C0C0C0C0C) >> 2;
+    let b = (x & 0x0303030303030303);
+    let p = G4_sq(a ^ b);
+    let q = G4_scl_N2(G4_sq(b));
+    return (p << 2) | q;
+}
+
+// inverse in GF(2^4), using normal basis (alpha^8,alpha^2)
+fn G16_inv(x: u64) -> u64 {
+    let a = (x & 0x0C0C0C0C0C0C0C0C) >> 2;
+    let b = (x & 0x0303030303030303);
+    let c = G4_scl_N(G4_sq(a ^ b));
+    let d = G4_mul(a, b);
+    let e = G4_inv(c ^ d);
+    let p = G4_mul(e, b);
+    let q = G4_mul(e, a);
+    return (p <<2 ) | q;
+}
+
+// inverse in GF(2^8), using normal basis (d^16,d)
+fn G256_inv(x: u64) -> u64 {
+    let a = (x & 0xF0F0F0F0F0F0F0F0) >> 4;
+    let b = (x & 0x0F0F0F0F0F0F0F0F);
+    let c = G16_sq_scl(a ^ b);
+    let d = G16_mul(a, b);
+    let e = G16_inv(c ^ d);
+    let p = G16_mul(e, b);
+    let q = G16_mul(e, a);
+    return (p << 4) | q;
+}
+
+fn helper(mut x: u64, b: &[u64, ..8]) -> u64 {
+    let mut i = 7;
+    let mut y = 0;
+    while i >= 0 {
+        if (x & 1) != 0 {
+            y ^= b[i];
+        }
+        x >>= 1;
+        i -= 1;
+    }
+    return y & 0xFF;
+}
+
+// convert to new basis in GF(2^8)
+// i.e., bit matrix multiply
+fn G256_newbasis(x: u64, k: &[u64, ..8]) -> u64 {
+    let a = helper((x & 0x00000000000000FF), k);
+    let b = helper((x & 0x000000000000FF00) >> 8, k) << 8;
+    let c = helper((x & 0x0000000000FF0000) >> 16, k) << 16;
+    let d = helper((x & 0x00000000FF000000) >> 24, k) << 24;
+    let e = helper((x & 0x000000FF00000000) >> 32, k) << 32;
+    let f = helper((x & 0x0000FF0000000000) >> 40, k) << 40;
+    let g = helper((x & 0x00FF000000000000) >> 48, k) << 48;
+    let h = helper((x & 0xFF00000000000000) >> 56, k) << 56;
+    let t = a | b | c | d | e | f | g | h;
+    return t;
+}
+
+// find Sbox of n in GF(2^8) mod POLY
+fn Sbox(n: u64) -> u64 {
+    let mut t = G256_newbasis(n, &A2X);
+    t = G256_inv(t);
+    t = G256_newbasis(t, &X2S);
+    return t ^ 0x6363636363636363;
+}
+
+// find inverse Sbox of n in GF(2^8) mod POLY
+fn iSbox(n: u64) -> u64 {
+    let mut t = G256_newbasis(n, &S2X);
+    t = G256_inv(t);
+    t = G256_newbasis(t, &X2A);
+    return t ^ 0x6363636363636363;
+}
+
+fn pack(a: u32, b: u32) -> u64 {
+    return (a as u64) << 32 | (b as u64);
+}
+
+fn unpack(x: u64, outa: &mut u32, outb: &mut u32) {
+    *outa = (x >> 32) as u32;
+    *outb = x as u32;
+}
